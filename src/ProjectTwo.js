00f2e81f0cdc116194c760f771a3552b2e1891a4
bridgemami <mdbridgeman@gmail.com>
2022-12-01 18:25:10 -0800

    commented out projectTwo

--- src/ProjectTwo.js
+++ src/ProjectTwo.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Header from './ComponentTwo/Header.jsx';
-import Body from './ComponentTwo/Body.jsx';
-function ProjectTwo () {
-    return (
-        <>
-        <Header />
-        <Body />
-        </>
-    )
-}
+// import React from 'react';
+// import Header from './ComponentTwo/Header.jsx';
+// import Body from './ComponentTwo/Body.jsx';
+// function ProjectTwo () {
+//     return (
+//         <>
+//         <Header />
+//         <Body />
+//         </>
+//     )
+// }
 
-export default ProjectTwo;
+// export default ProjectTwo;
